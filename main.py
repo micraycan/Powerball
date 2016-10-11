8d07133725295db01f90c70d3ee8182b3bab187e
MICHAEL <MICRAYCAN@GMAILCOM>
2016-10-10 23:34:09 -0400

    tabs to spaces

--- main.py
+++ main.py
@@ -12,35 +12,35 @@ all_nums = []
 all_pb = []
 
 for row in table:
-	# just in case date is needed in the future
-	# date = row.get_text()[0:16]
-	num = row.get_text()[16:31]
-	pb = row.get_text()[31:]
+    # just in case date is needed in the future
+    # date = row.get_text()[0:16]
+    num = row.get_text()[16:31]
+    pb = row.get_text()[31:]
 
-	num_list = num.split()
-	all_nums.append(num_list)
+    num_list = num.split()
+    all_nums.append(num_list)
 
-	all_pb.append(pb)
+    all_pb.append(pb)
 
 # create dictionary to count most common numbers
 num_counter = {}
 
 for numbers in all_nums:
-	for n in numbers:
-		if n in num_counter:
-			num_counter[n] += 1
-		else:
-			num_counter[n] = 1
+    for n in numbers:
+        if n in num_counter:
+            num_counter[n] += 1
+        else:
+            num_counter[n] = 1
 
 # create another dictionary for powerball numbers
 pb_counter = {}
 
 for ball in all_pb:
-	for p in ball:
-		if p in pb_counter:
-			pb_counter[p] += 1
-		else:
-			pb_counter[p] = 1
+    for p in ball:
+        if p in pb_counter:
+            pb_counter[p] += 1
+        else:
+            pb_counter[p] = 1
 
 # sort the dictionary keys by value in descending order
 ordered_num = sorted(num_counter, key = num_counter.__getitem__, reverse = True)
